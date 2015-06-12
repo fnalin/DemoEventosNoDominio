f751bb0d9f85445885a36b3471fbac450ec5a13b
fnalin@msn.com <fnalin@msn.com>
2015-06-12 17:30:45 -0300

    Subindo

---
+++ DemoEventosNoDominio.Domain/Entidade/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DemoEventosNoDominio.Domain.Entidade
+{
+    public delegate void ControleDeIdadeEventHandler(object source, EventArgs e);
+
+    public class Pessoa
+    {
+        public Pessoa(string nome, int idade)
+        {
+            ID = Guid.NewGuid();
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public event ControleDeIdadeEventHandler ExcedeuIdadeLimite;
+
+        private readonly int _idadeMinima = 18;
+        private int _idade;
+
+        public Guid ID { get; private set; }
+        public string Nome { get; private set; }
+        
+        public int Idade
+        {
+            get
+            {
+                return _idade;
+            }
+            private set
+            {
+                if (value < _idadeMinima)
+                    OnIdadeExcedida(new EventArgs());
+
+                _idade = value;
+            }
+        }
+
+
+        public virtual void OnIdadeExcedida(EventArgs e)
+        {
+            if (ExcedeuIdadeLimite != null)
+                ExcedeuIdadeLimite(this, e);
+        }
+
+        public void AlterarIdade(int idade)
+        {
+            this.Idade = idade;
+        }
+    }
+}
---
+++ DemoEventosNoDominio.UIConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using DemoEventosNoDominio.Domain.Entidade;
+using System;
+
+namespace DemoEventosNoDominio.UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa("Fabiano", 35);
+            
+            pessoa.ExcedeuIdadeLimite += new ControleDeIdadeEventHandler((object source, EventArgs e) => {
+                Console.WriteLine("Idade não permitida");
+            });
+
+
+            pessoa.AlterarIdade(15);
+
+            Console.ReadLine();
+
+        }
+    }
+}
